e4bd9c02b52142ac670fddd137628ce871452a58
Aayush Adhikari <aayushadhikari601@gmail.com>
2024-02-19 23:55:48 +0545

    after middleware

--- Front_end/joyfull/src/App.jsx
+++ Front_end/joyfull/src/App.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Intro from './Components/Intro/Intro';
-
 import Mission from './Components/Mission/Mission';
 import About from './Components/About/About';
 import Story from './Components/Story/Story';
 import Banner from './Components/Banner/Banner';
-
 import Testimonial from './Components/Testimonial/Testimonial';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
 import Register from './Components/Register/Register';
-import Main from './Components/Main/Main'
+import Main from './Components/Main/Main';
 import './App.css';
 import Login from './Components/Login/Login';
 import InnerAbout from './Components/InnerAbout/InnerAbout';
 import InnerContact from './Components/InnerContact/InnerContact';
-
+import Protect from './Components/Protect/Protect';
 
 const App = () => {
   return (
@@ -38,16 +36,15 @@ const App = () => {
                 <Testimonial />
                 <Contact />
                 <Footer />
-                
               </>
             }
           />
           <Route path="/register" element={<Register />} />
-          <Route path = "/Login" element={<Login />} />
-          <Route path = "/main" element={<Main />} />
-          <Route path = "/innerAbout" element={<InnerAbout/>} />
-          <Route path = "/innercontact" element={<InnerContact/>} />
-
+          <Route path="/login" element={<Login />} />
+          {/* Wrap the protected routes with Protect component */}
+          <Route path="/main" element={<Protect><Main /></Protect>} />
+          <Route path="/innerAbout" element={<Protect><InnerAbout /></Protect>} />
+          <Route path="/innercontact" element={<Protect><InnerContact /></Protect>} />
         </Routes>
       </div>
     </Router>
--- Front_end/joyfull/src/Components/Protect/Protect.jsx
+++ Front_end/joyfull/src/Components/Protect/Protect.jsx
@@ -2,14 +2,13 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const Protect = () => {
+const Protect = ({ children }) => {
     const navigateTo = useNavigate();
 
     useEffect(() => {
         const checkAuth = async () => {
             try {
                 await axios.get('http://localhost:5000/protected', { withCredentials: true });
-             
             } catch (error) {
                 console.error(error);
                 navigateTo('/login');
@@ -19,7 +18,7 @@ const Protect = () => {
         checkAuth();
     }, [navigateTo]);
 
-    return null; // No need for any UI rendering
+    return <>{children}</>; // Render the child components if authenticated
 };
 
 export default Protect;
